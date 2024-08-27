99bdffa44262633abbed8eb62acd859e099700e1
Juan Antero Asumu <149675635+Vanila-Hub@users.noreply.github.com>
2024-08-27 10:54:39 +0200

    Primera Ventana

---
+++ front-end/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Body } from "./Home_components/Body"
+
+export const Home = () => {
+  return (
+    <>
+        <Body/>
+    </>
+  )
+}
---
+++ front-end/src/pages/Home/Home_components/Header.tsx
@@ -0,0 +1,12 @@
+import { Navbarr } from "./Navbar";
+
+
+export const Header = () => {
+  return (
+    <>
+      <header className="w-full flex flex-row ">
+        <Navbarr />
+      </header>
+    </>
+  );
+};
