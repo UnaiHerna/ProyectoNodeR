c98d959580793889caa04ef25640df6ec1bce732
Juan Antero Asumu <149675635+Vanila-Hub@users.noreply.github.com>
2024-12-20 11:10:24 +0100

    gg

--- Mock-up/mock-up-cimicoppx/src/components/Home/Organism/ScatterChart2.tsx
+++ Mock-up/mock-up-cimicoppx/src/components/Home/Organism/ScatterChart2.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import {
   ScatterChart,
   Scatter,
@@ -46,7 +46,7 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
       <g transform={`translate(${x},${y - 15})`}>
         <foreignObject x={-40} y={-25} width={88} height={30} stroke="red">
           <div className="flex justify-center items-center w-full h-full">
-            {payload.value == 4 && showLine && activeDataType!=="tube" ? (
+            {payload.value == 4 && showLine && activeDataType !== "tube" ? (
               <SensorButton
                 label={label}
                 className="rounded-md self-start w-16 bg-cimico text-white" // Color cambiado aquí
@@ -145,20 +145,22 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
   }) => {
     const { x, y, metrics, payload } = props;
     const label = metrics[payload.value];
-
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <foreignObject x={-1} y={-17} width={80} height={30}>
-          <div className="flex text-start items-center w-full h-full">
-            <span className="text-[#002060] font-lato text-[11pt]">
-              {label}
-            </span>
-          </div>
-        </foreignObject>
-      </g>
-    );
+    if (showPoints) {
+      return (
+        <g transform={`translate(${x + 6},${y})`}>
+          <foreignObject x={-1} y={-17} width={80} height={30}>
+            <div className="flex text-start items-center w-full h-full">
+              <span className="text-[#002060] font-lato text-[11pt]">
+                {label}
+              </span>
+            </div>
+          </foreignObject>
+        </g>
+      );
+    } else {
+      return <g transform={`translate(${x},${y})`}></g>;
+    }
   };
-
   // Custom tick for Y-axis (using metrics)
   const CustomXAxisTick2 = (props: {
     x: number;
@@ -194,9 +196,9 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
       return (
         <div className="bg-[#fee8b4] p-2 rounded opacity-80 w-[7rem] text-start">
           {/* Hora */}
-          <div className="text-[#002060] text-[10pt]">{label}:00</div>
+          <div className="text-[#002060] text-[13pt]">{label}:00</div>
           {/* Valor + Unidad */}
-          <div className="text-[#002060] text-[10pt]">
+          <div className="text-[#002060] text-[13pt]">
             {`${payload[0]?.value} ppm`}
           </div>
         </div>
@@ -214,25 +216,27 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
     RINT: 0,
   };
 
+  const unidades_tube = ["L/h", "m³/h", "m³/h", "m³/h"];
+
   const data_dumbbell = [
-    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 2 },
-    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 1.3 },
-    { var: tubeRawLabels[rowIndexMap["SURP"]], y: 1, x: 8 },
-    { var: tubeRawLabels[rowIndexMap["FeCl3"]], y: 0, x: 6 },
+    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 2, value: null },
+    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 1, value: null },
+    { var: tubeRawLabels[rowIndexMap["SURP"]], y: 1, x: 8, value: 65 },
+    { var: tubeRawLabels[rowIndexMap["FeCl3"]], y: 0, x: 6, value: 23 },
   ];
 
   const data_dumbbell2 = [
-    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 6 },
-    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 8 },
+    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 6, value: 2915 },
+    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 8, value: 1235 },
   ];
 
   const linea_RINT = [
-    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 2 },
-    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 6 },
+    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 2, value: 70 },
+    { var: tubeRawLabels[rowIndexMap["RINT"]], y: 3, x: 6, value: 80 },
   ];
-  const linea_RAS= [
-    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 1.3 },
-    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 8 },
+  const linea_RAS = [
+    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 1, value: 90 },
+    { var: tubeRawLabels[rowIndexMap["RAS"]], y: 2, x: 8, value: 100 },
   ];
 
   return (
@@ -322,7 +326,6 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
                   )}
                 />
               )}
-              <Tooltip cursor={{ strokeDasharray: "3 3" }} />
               <Scatter
                 name="Data Points"
                 data={data}
@@ -333,12 +336,12 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
           )
         ) : (
           //esto es si es tube chart
-            <ComposedChart margin={{ top: 20, right: 10, left: 12, bottom: 50 }}>
+          <ComposedChart margin={{ top: 20, right: 0, left: 12, bottom: 50 }}>
             <CartesianGrid strokeDasharray="0 0" stroke="#f2f2f2" />
             <XAxis
               dataKey="x"
               type="number"
-              ticks={[0, 1, 2, 3, 4, 5, 6, 7,8]}
+              ticks={[0, 1, 2, 3, 4, 5, 6, 7, 8]}
               axisLine={false}
               tickLine={false}
               tick={(props) => <CustomXAxisTick {...props} />}
@@ -354,11 +357,25 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
               tickLine={false}
               tick={(props) => <CustomYAxisTick3 {...props} />}
             />
+
+            <YAxis
+              dataKey="y"
+              type="number"
+              domain={[4, 0]}
+              ticks={[3, 2, 1, 0]}
+              orientation="right"
+              yAxisId={"right"}
+              axisLine={false}
+              tickLine={false}
+              tick={(props) => (
+                <CustomYAxisTick2 {...props} metrics={unidades_tube} />
+              )}
+            />
             <Line
               type="bump"
               dataKey="y"
               data={linea_RINT}
-              stroke="#082464"
+              stroke={showPoints ? "#f8f4f4" : "#082464"}
               strokeWidth={5} // Set the line width here
               dot={false} // Hide the dot
             />
@@ -366,20 +383,96 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({
               type="bump"
               dataKey="y"
               data={linea_RAS}
-              stroke={showPoints?"#f8f4f4":"#082464"}
+              stroke={showPoints ? "#f8f4f4" : "#082464"}
               fill="white"
               dot={false} // Hide the dot
               strokeWidth={5} // Set the line width here
             />
-            <Scatter name="Data Points" data={data_dumbbell} shape={(props:ScatterPointItem) => <><circle cx={props.cx} cy={props.cy} r={10} fill="white" /><circle cx={props.cx} cy={props.cy} r={6} fill="#082464" /></>}/>
-            <Scatter name="Data Points" data={data_dumbbell2} shape={(props:ScatterPointItem) => <><circle cx={props.cx} cy={props.cy} r={10} fill="#082464" /><circle cx={props.cx} cy={props.cy} r={6} fill="white" /></>}/>
-            </ComposedChart>
+            <Scatter
+              name="Data Points"
+              data={data_dumbbell}
+              shape={(props: ScatterPointItem) => (
+                <>
+                
+                  <circle cx={props.cx} cy={props.cy} r={10} fill="white" />
+                  <circle
+                    cx={props.cx}
+                    cy={props.cy}
+                    r={6}
+                    fill={showPoints ? "#f8f4f4" : "#082464"}
+                  />
+                  {/* Fondo (rect) centrado con el texto */}
+                  {showPoints&&(<>
+                  {props.payload.value && (
+                    <rect
+                    x={props.cx ? props.cx  - 50 : 0} // Centrar el rect con respecto al texto
+                    y={props.cy ? props.cy - 40 : 0} // Centrar verticalmente el rect
+                    width={60} // Ancho del fondo
+                    height={60} // Alto del fondo
+                    fill="white" // Color del fondo
+                    stroke="none" // Sin borde
+                    rx={10} // Bordes redondeados
+                  />
+                  )}
+                {/* Texto sobre el fondo */}
+                <text
+                  x={props.cx? props.cx-3 : 0} // Texto centrado horizontalmente
+                  y={props.cy? props.cy+5: 0} // Texto centrado verticalmente
+                  textAnchor="middle"
+                  fill="#002060"
+                  fontFamily="Lato"
+                  fontSize="17pt"
+                >{props.payload.value}</text>
+                  </>)}
+                </>
+              )}
+            />
+            <Scatter
+              name="Data Points"
+              data={data_dumbbell2}
+              shape={(props: ScatterPointItem) => (
+                <>
+                  <circle
+                    cx={props.cx}
+                    cy={props.cy}
+                    r={10}
+                    fill={showPoints ? "#f8f4f4" : "#082464"}
+                  />
+                  <circle cx={props.cx} cy={props.cy} r={6} fill="white" />
+                  {/* Fondo (rect) centrado con el texto */}
+                  {showPoints && (
+                    <>
+                      <rect
+                        x={props.cx ? props.cx / 1.5 - 40 : 0} // Centrar el rect con respecto al texto
+                        y={props.cy ? props.cy - 20 : 0} // Centrar verticalmente el rect
+                        width={80} // Ancho del fondo
+                        height={60} // Alto del fondo
+                        fill="white" // Color del fondo
+                        stroke="none" // Sin borde
+                        rx={10} // Bordes redondeados
+                      />
+                      {/* Texto sobre el fondo */}
+                      <text
+                        x={props.cx ? props.cx / 1.5 : 0} // Texto centrado horizontalmente
+                        y={props.cy ? props.cy + 5 : 0} // Texto centrado verticalmente
+                        textAnchor="middle"
+                        fill="#002060"
+                        fontFamily="Lato"
+                        fontSize="17pt"
+                      >
+                        {props.payload.value}
+                      </text>
+                    </>
+                  )}
+                </>
+              )}
+            />
+          </ComposedChart>
         )}
       </ResponsiveContainer>
     </div>
   );
 };
-
 
 // Custom scatter shape function para el primer grafico
 const CustomScatterShape = (props: ScatterPointItem) => {
@@ -394,9 +487,9 @@ const CustomScatterShape = (props: ScatterPointItem) => {
     <>
       {/* Fondo blanco detrás del texto */}
       <rect
-        x={cx} // Ajusta la posición horizontal
-        y={cy} // Ajusta la posición vertical
-        width={30} // Ancho del fondo
+        x={cx - 16} // Ajusta la posición horizontal
+        y={cy - 13} // Ajusta la posición vertical
+        width={40} // Ancho del fondo
         height={20} // Alto del fondo
         fill="white" // Color del fondo
         stroke="none" // Sin borde
